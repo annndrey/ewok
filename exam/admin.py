0ec1ed316712e325a0261c90733675df2bddf33c
annndrey@ewok <gontchar@gmail.com>
2017-11-01 21:43:13 +0300

    добавили кнопку выгрузки результатов в админке для выбранных групп

--- exam/admin.py
+++ exam/admin.py
@@ -1,11 +1,78 @@
 # encoding: utf-8
-# Register your models here.
+
+import psycopg2
+import sqlalchemy
+import csv
+import pandas as pd
+from numpy import nanmean
+from StringIO import StringIO
+from sqlalchemy import create_engine, func
+from sqlalchemy.orm import sessionmaker
 
 from django import forms
+from django.http import HttpResponse
 from django.contrib import admin
 from django.db.models import Count
 from .models import Test, Question, Variant, TestResult, Student, StudentGroup, Teacher
 from .forms import TeacherForm
+
+
+def connect(user, password, db, host='localhost', port=5432):
+    url = 'postgresql://{}:{}@{}:{}/{}'
+    url = url.format(user, password, host, port, db)
+    con = sqlalchemy.create_engine(url, client_encoding='utf8')
+    meta = sqlalchemy.MetaData(bind=con, reflect=True)
+    return con, meta
+
+
+def meanrow(row):
+    numvalues = []
+    for x in row:
+        try:
+            numvalues.append(float(x))
+        except:
+            pass
+    if len(numvalues) > 0:    
+        return nanmean(numvalues)
+    else:
+        return None
+
+    
+def get_group_results(modeladmin, request, queryset):
+    response = HttpResponse(content_type='text/csv; charset=utf-8')
+    response['Content-Disposition'] = 'attachment; filename="groupresults.csv"'
+    writer = csv.writer(response)
+
+    con, meta = connect('exam', 'exam', 'exam')
+    Session = sessionmaker(bind=con)
+    session = Session()
+    tests = meta.tables['exam_test']
+    students = meta.tables['exam_student']
+    groups = meta.tables['exam_studentgroup']
+    testresults = meta.tables['exam_testresult']
+    groupnames = [g.name for g in queryset.all()]
+    allresults = session.query(students.c.surname, students.c.name, students.c.middlename, tests.c.title, testresults.c.result, groups.c.name).filter(groups.c.name.in_(groupnames)).join(testresults).join(tests).join(groups)
+            
+    outcsv = []
+    for i, r in enumerate(allresults):
+        for k in r[4].keys():
+            row = u"""{0} {1} {2};{5};Тест "{3}";{4}""".format(r[0], r[1], r[2], r[3], (u"%s;%.2f" % (k, r[4][k]) if isinstance(r[4][k], int) else u"%s;%s" % (k,r[4][k].values()[0])), r[5])
+            outcsv.append(row)
+    outcsv = "\n".join(outcsv)
+
+    df = pd.read_csv(StringIO(outcsv), sep=';', header=None, names=['name', 'group', 'test', 'resname', 'resvalue'])
+    cols = ['test', 'name', 'group', 'resname', 'resvalue']
+    df = df[cols]
+    dfgroupped = df.groupby(['test', 'resname', 'group', 'name'])['resvalue'].sum().unstack(['group', 'name'])
+    summary_ave_data = dfgroupped.copy()
+    summary_ave_data['average'] = summary_ave_data.apply(meanrow, axis=1)
+
+    summary_ave_data.to_csv(response , sep=';')
+
+    return response
+
+get_group_results.short_description = "Скачать результаты"
+
 
 class DeleteNotAllowedModelAdmin(admin.ModelAdmin):
     def has_delete_permission(self, request, obj=None):
@@ -55,6 +122,7 @@ class TeacherAdmin(admin.ModelAdmin):
 class StudentGroupAdmin(admin.ModelAdmin):
     list_display = ('name', 'stcount')
     list_filter = ('name',)
+    actions = [get_group_results,]
 
 @admin.register(Test)
 class TestAdmin(admin.ModelAdmin):
